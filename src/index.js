53c7e3bff1d0a997a6054b2203c3fe6b803ba027
Aastha <saxena.aastha91@gmail.com>
2023-08-13 22:28:25 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const li = document.createElement("li");
+    li.innerHTML = e.target["new-task-description"].value
+    document.getElementById("tasks").append(li)
+  })
 });
